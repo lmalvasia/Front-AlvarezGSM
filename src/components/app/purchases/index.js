15c04ea07eec2a297201900bc94fe972582983d1
Brian Sangermano <briansangermano@gmail.com>
2019-03-01 14:55:48 -0300

    add index and view for component-purchases

--- src/components/app/purchases/index.js
+++ src/components/app/purchases/index.js
@@ -1,3 +1,22 @@
 import Purchases from "./view";
+import { connect } from "react-redux";
+import { getPurchasesThunk } from "../../../actions/purchases";
 
-export default Purchases;
+const mapStateToProps = state => ({
+  providers: state.purchases.list,
+  isLoading: state.purchases.isLoading,
+  errorMsg: state.purchases.errorMsg,
+  successMsg: state.purchases.successMsg
+});
+
+const mapDispatchToProps = dispatch => ({
+  getPurchases: () => {
+    dispatch(getPurchasesThunk());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Purchases);
+
--- src/components/app/purchases/view.js
+++ src/components/app/purchases/view.js
@@ -5,12 +5,46 @@ import React, { Component } from "react";
 import "./style.css";
 
 class Purchases extends Component {
+  componentWillMount() {
+    this.props.getPurchases();
+  }
+  getPurchasesList = () => {
+    const { purchases } = this.props;
+    return purchases.map(purchase => (
+      <tr key={purchase._id}>
+        <th scope="row" />
+        <td>{purchase.purchase_number}</td>
+        <td>{purchase.provider}</td>
+        <td className="update" />
+        <td className="delete" />
+      </tr>
+    ));
+  };
   render() {
     return (
-      <div className="Purchases">
-        <div className="content">
-          <h1>Purchases</h1>
+      <div className="content">
+        <div className="row">
+          <div className="col-6">
+            <h1>Purchases</h1>
+          </div>
+          <div className="col-6 text-right">
+            <a href="/addPurchase" className="btn btn-primary">
+              Add Purchase
+            </a>
+          </div>
         </div>
+        <table className="table table-sm table-hover">
+          <thead className="thead-light">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Purchase Number</th>
+              <th scope="col">Provider</th>
+              <th scope="col" />
+              <th scope="col" />
+            </tr>
+          </thead>
+          <tbody>{this.getPurchasesList()}</tbody>
+        </table>
       </div>
     );
   }
