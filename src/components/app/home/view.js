ecb0c21c5abe92fbd0bf163d5efc40207ef11488
Brian Sangermano <briansangermano@gmail.com>
2019-03-01 14:04:13 -0300

    add style from component-home

--- src/components/app/home/view.js
+++ src/components/app/home/view.js
@@ -7,12 +7,18 @@ import "./style.css";
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
+      <div className="Home text-center">
         <div className="content">
-          <h2>Bienvenido al sitio web</h2>
+          <h2>"BIENVENIDO AL SITIO WEB"</h2>
         </div>
         <div className="tagline">
-          <p>Welcome</p>
+            <h3>Descripción:</h3>
+            <p>Servicio técnico especializado en reparación de celulares, tablets y computadoras. Liberaciones y desbloqueos. 
+            Venta de equipos y accesorios.</p>
+        </div>
+        <div className="info">
+          <p><spam>Ubicacion:</spam> 9 de Julio 753 (2107 Alvarez)</p>
+          <p><spam>Telefono:</spam> 3402-458393</p>
         </div>
       </div>
     );
