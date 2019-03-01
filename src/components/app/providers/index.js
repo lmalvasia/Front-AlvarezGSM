0b8c8c59634bbfd66097cc4093c2bcaf6062e08e
Brian Sangermano <briansangermano@gmail.com>
2019-03-01 10:57:40 -0300

    add index and view for component-provider

--- src/components/app/providers/index.js
+++ src/components/app/providers/index.js
@@ -1,3 +1,21 @@
 import Providers from "./view";
+import { connect } from "react-redux";
+import { getProvidersThunk } from "../../../actions/providers";
 
-export default Providers;
+const mapStateToProps = state => ({
+  providers: state.providers.list,
+  isLoading: state.providers.isLoading,
+  errorMsg: state.providers.errorMsg,
+  successMsg: state.providers.successMsg
+});
+
+const mapDispatchToProps = dispatch => ({
+  getProviders: () => {
+    dispatch(getProvidersThunk());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Providers);
--- src/components/app/providers/view.js
+++ src/components/app/providers/view.js
@@ -5,16 +5,53 @@ import React, { Component } from "react";
 import "./style.css";
 
 class Providers extends Component {
+  componentWillMount() {
+    this.props.getProviders();
+  }
+  getProvidersList = () => {
+    const { providers } = this.props;
+    return providers.map(provider => (
+      <tr key={provider._id}>
+        <th scope="row" />
+        <td>{provider.company}</td>
+        <td>{provider.email}</td>
+        <td>{provider.street}</td>
+        <td>{provider.cellphone}</td>
+        <td className="update" />
+        <td className="delete" />
+      </tr>
+    ));
+  };
   render() {
     return (
-      <div className="Providers">
-        <div className="content">
-          <h1>Providers</h1>
+      <div className="content">
+        <div className="row">
+          <div className="col-6">
+            <h1>Repuestos</h1>
+          </div>
+          <div className="col-6 text-right">
+            <a href="/addProvider" className="btn btn-primary">
+              Add Provider
+            </a>
+          </div>
         </div>
+        <table className="table table-sm table-hover">
+          <thead className="thead-light">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Company</th>
+              <th scope="col">Email</th>
+              <th scope="col">Street</th>
+              <th scope="col">Cellphone</th>
+              <th scope="col" />
+              <th scope="col" />
+            </tr>
+          </thead>
+          <tbody>{this.getProvidersList()}</tbody>
+        </table>
       </div>
     );
   }
 }
 
 export default Providers;
-
